464c067f6adce42b19a306e0e4bcf03896ad3902
mdubelbeis <mason.dubelbeis@gmail.com>
2022-10-21 16:33:20 -0500

    restructured code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import PlayerFourReducer from "./commander/PlayerFour";
+import PlayerThreeReducer from "./commander/PlayerThree";
+import PlayerTwoReducer from "./commander/PlayerTwo";
+import PlayerOneReducer from "./commander/PlayerOne";
 
 export const store = configureStore({
   reducer: {
-    playerOne: playerOneReducer;
-    playerTwo: playerTwoReducer;
-    playerThree: playerThreeReducer;
-    playerFour: playerFourReducer;
+    PlayerFour: PlayerFourReducer,
+    PlayerThree: PlayerThreeReducer,
+    PlayerTwo: PlayerTwoReducer,
+    PlayerOne: PlayerOneReducer,
   },
 });
