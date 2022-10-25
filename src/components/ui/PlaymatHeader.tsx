1c223b9da2fd919ea7f77aebdc262b204bc2ac38
mdubelbeis <mason.dubelbeis@gmail.com>
2022-10-24 21:00:29 -0500

    refactored

--- src/components/ui/PlaymatHeader.tsx
+++ src/components/ui/PlaymatHeader.tsx
@@ -1,27 +1,10 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { BiHome } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { IoRefresh } from "react-icons/io5";
-import { BiHome } from "react-icons/bi";
-import ApplyButton from "./ApplyButton";
 import DamageCounters from "../PlaymatHeader/DamageCounters";
-
-import {
-  updatePlayerOneBgColor,
-  updatePlayerOneTextColor,
-} from "../../app/commander/PlayerOneSlice";
-import {
-  updatePlayerTwoBgColor,
-  updatePlayerTwoTextColor,
-} from "../../app/commander/PlayerTwoSlice";
-import {
-  updatePlayerThreeBgColor,
-  updatePlayerThreeTextColor,
-} from "../../app/commander/PlayerThreeSlice";
-import {
-  updatePlayerFourBgColor,
-  updatePlayerFourTextColor,
-} from "../../app/commander/PlayerFourSlice";
+import BackgroundColorMenu from "../PlaymatHeader/BackgroundColorMenu";
+import TextColorMenu from "../PlaymatHeader/TextColorMenu";
 
 interface PlaymatHeaderProps {
   commanderDamage: number;
@@ -36,9 +19,8 @@ const PlaymatHeader: React.FC<PlaymatHeaderProps> = ({
   poisonDamage,
   commanderDamage,
 }) => {
-  const [newBgColor, setNewBgColor] = useState<string>("");
-  const [newTextColor, setNewTextColor] = useState<string>("");
   const dispatch = useDispatch();
+  // const [newTextColor, setNewTextColor] = useState<string>("");
 
   // TODO: CANI? Get commander name, query the API, get an img and set game-board background???
   const getPlayer = () => {
@@ -54,56 +36,12 @@ const PlaymatHeader: React.FC<PlaymatHeaderProps> = ({
     }
   };
 
-  const handleBgColorApply = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    switch (player) {
-      case "PlayerOne":
-        dispatch(updatePlayerOneBgColor(newBgColor));
-        break;
-      case "PlayerTwo":
-        dispatch(updatePlayerTwoBgColor(newBgColor));
-        break;
-      case "PlayerThree":
-        dispatch(updatePlayerThreeBgColor(newBgColor));
-        break;
-      case "PlayerFour":
-        dispatch(updatePlayerFourBgColor(newBgColor));
-        break;
-      default:
-        console.log("There is an error...");
-    }
-  };
-
-  const handleTextColorApply = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    switch (player) {
-      case "PlayerOne":
-        dispatch(updatePlayerOneTextColor(newTextColor));
-        break;
-      case "PlayerTwo":
-        dispatch(updatePlayerTwoTextColor(newTextColor));
-        break;
-      case "PlayerThree":
-        dispatch(updatePlayerThreeTextColor(newTextColor));
-        break;
-      case "PlayerFour":
-        dispatch(updatePlayerFourTextColor(newTextColor));
-        break;
-      default:
-        console.log("There is an error...");
-    }
-  };
-
   const handleCommanderSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
 
-  const handleCommanderDamageApply = (
-    e: React.ChangeEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
+  const handleRefreshGame = () => {
+    // Dispatch to each player, resetting lifeTotal, commander damage, poison damage
   };
 
   return (
@@ -113,10 +51,8 @@ const PlaymatHeader: React.FC<PlaymatHeaderProps> = ({
         opacity === "0" ? "hidden" : ""
       } top-0 left-0 bg-[#2c3142] w-full text-white p-4 h-full flex flex-col items-center gap-6 max-w-lg`}
     >
-      <div
-        className="absolute top-4 left-6"
-        onClick={() => console.log("Refreshed")}
-      >
+      {/* LINKS */}
+      <div className="absolute top-4 left-6" onClick={handleRefreshGame}>
         <IoRefresh className="w-10 h-10 hover:cursor-pointer" />
       </div>
       <div className="absolute top-4 right-6">
@@ -151,75 +87,13 @@ const PlaymatHeader: React.FC<PlaymatHeaderProps> = ({
         id="settings-wrapper"
         className="grid grid-cols-1 gap-2 p-4 w-full"
       >
-        {/* CHANGE BG COLOR */}
-        <div
-          id="section-bg-color"
-          className="flex items-center justify-between gap-4 h-min p-2 rounded text-[#2c3142] bg-white w-full"
-        >
-          <h3 className="font-bold">Playmat: </h3>
-          <form
-            onSubmit={handleBgColorApply}
-            className="flex items-center justify-between gap-4 w-full"
-          >
-            <label>
-              <select
-                name="bg-color"
-                id="bg-color"
-                className="py-2 px-4 rounded bg-slate-50"
-                onChange={(e) => setNewBgColor(e.target.value)}
-              >
-                <option value="bg-red-500">Red</option>
-                <option value="bg-blue-500" selected>
-                  Blue
-                </option>
-                <option value="bg-green-500">Green</option>
-                <option value="bg-black">Black</option>
-                <option value="bg-slate-100">Colorless</option>
-              </select>
-            </label>
-            <ApplyButton />
-          </form>
-        </div>
-
-        {/* CHANGE TEXT COLOR */}
-        <div
-          id="section-text-color"
-          className="flex items-center justify-between gap-4 h-min p-2 rounded text-[#2c3142] bg-white w-full"
-        >
-          <h3 className="font-bold">Text</h3>
-          <form
-            onSubmit={handleTextColorApply}
-            className="flex items-center justify-between gap-4 w-full"
-          >
-            <label>
-              <select
-                name="text-color"
-                id="text-color"
-                className="py-2 px-4 rounded bg-slate-50"
-                onChange={(e) => setNewTextColor(e.target.value)}
-              >
-                <option value="bg-yellow-500">Yellow</option>
-                <option value="bg-blue-800">Dark Blue</option>
-                <option value="bg-green-800">Dark Green</option>
-                <option value="bg-black" selected>
-                  Black
-                </option>
-                <option value="bg-slate-500">Colorless</option>
-                <option value="bg-white">White</option>
-              </select>
-            </label>
-            <ApplyButton />
-          </form>
-        </div>
-
-        {/* CHANGE COMMANDER DAMAGE */}
+        <BackgroundColorMenu player={player} />
+        <TextColorMenu player={player} />
         <DamageCounters
           title="Commander Damage"
           player={player}
           counter={commanderDamage}
         />
-
-        {/* CHANGE POISON DAMAGE */}
         <DamageCounters
           title="Poison Damage"
           player={player}
