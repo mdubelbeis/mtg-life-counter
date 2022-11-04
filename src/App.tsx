cad645ea1dd13a0137f41f795087f7c6d1d92214
mdubelbeis <mason.dubelbeis@gmail.com>
2022-11-04 02:56:45 -0500

    added h and w to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
   const innerHeight = window.innerHeight;
   return (
-    <div className="h-[innerHeight + 'px']">
+    <div className={`h-[${innerHeight + "px"}] w-screen`}>
       {/* <Header /> */}
       <Outlet />
     </div>
