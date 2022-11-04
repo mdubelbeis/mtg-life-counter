055663aaebd6070e90fdc4828b4233e2dc47b0c9
mdubelbeis <mason.dubelbeis@gmail.com>
2022-11-04 03:02:28 -0500

    adjust height

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const App: React.FC = () => {
   const innerHeight = window.innerHeight;
   return (
-    <div className={`h-full w-screen`}>
+    <div className={`min-h-screen min-w-screen`}>
       <Outlet />
     </div>
   );
